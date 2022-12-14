9aedd7244b067ecc9223d69ed5da204672cf86ab
Miguel Ángel <miguel.agudelo@inti.solutions>
2022-12-14 09:50:58 -0500

    Se agregan properties de conexión a db

--- src/main/java/demo/lottery/LotteryApplication.java
+++ src/main/java/demo/lottery/LotteryApplication.java
@@ -8,6 +8,7 @@ public class LotteryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LotteryApplication.class, args);
+		System.out.println("Sesión Iniciada");
 	}
 
 }
